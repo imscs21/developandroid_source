a5d9c10a4ca9d9d6a0157ba5aefa791b1c70a7dd
hwangsehyun <imscs21@naver.com>
2013-03-24 23:57:10 +0900

    day1 resource
    어플설치유무와 어플안에서 다른어플 실행소스

---
+++ checkinstalledpackage.java
@@ -0,0 +1,16 @@
+﻿// Package 설치여부 확인
+
+
+  PackageManager pm = getPackageManager();
+  try {
+       pm.getApplicationInfo("찾을 패키지명",PackageManager.GET_META_DATA);
+
+       //패키지가 있을경우 실행할 내용
+       }
+
+ catch (NameNotFoundException e)
+
+      {
+
+      //패키지가 없을경우 실행할 내용
+      }
---
+++ runanotherapp.java
@@ -0,0 +1,16 @@
+﻿public class Test extends Activity {
+	private Intent intent;
+	//실행할 다른 앱의 패키지명
+	private final String appPackageName = "com.grayd.book";   
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //intent에 패키지명 설정 string type
+        intent = this.getPackageManager().getLaunchIntentForPackage(appPackageName);
+        //다른앱을 실행
+        startActivity(intent);
+    }
+}
+
